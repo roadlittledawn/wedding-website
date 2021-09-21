0ef6374018a3cfc9d9cf55d0fa228435a1461d61
Clinton <clinton.langosch@gmail.com>
2021-09-20 21:17:16 -0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Brie and Clinton Wedding Website",
+  },
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
