57418fc00b10d9d34022e268de4936a8afcb84da
Clinton <clinton.langosch@gmail.com>
2021-09-28 23:55:53 -0700

    steal the knot graphics and make image polaroid

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ const HomePage = () => {
         <div
           css={css`
             display: flex;
+            flex-wrap: wrap;
             justify-content: space-around;
             align-items: center;
             > * {
@@ -30,41 +31,78 @@ const HomePage = () => {
               font-size: 2em;
             `}
           >
-            <div>June 4, 2022</div>
-            <div>
-              Abbey Road Farm <br />
-              Carlton, OR
+            <div
+              css={css`
+                width: 500px;
+                min-height: 530px;
+                display: flex;
+                align-items: center;
+                flex-direction: row;
+                padding: 3rem 1rem;
+                margin: 0 auto;
+                background: transparent;
+                height: calc(100% - 10rem);
+              `}
+            >
+              <div
+                css={css`
+                  text-align: center;
+                `}
+              >
+                <h1>
+                  <img
+                    src="https://flourish.theknot.com/lockup/initials?firstName=Brie&amp;fianceFirstName=Clinton&amp;themeId=1226&amp;liteSite=true"
+                    alt="Brie &amp; Clinton"
+                    class="css-1uqsgts"
+                  />
+                </h1>
+
+                <div>June 4, 2022</div>
+                <div>Carlton, OR</div>
+              </div>
             </div>
           </div>
           <div
             css={css`
-              width: 400px;
               position: relative;
               overflow: hidden;
-              /* :after {
-                content: "";
-                position: absolute;
-                transform: translate(-50%, -52%);
-                top: 50%;
-                left: 50%;
-                width: 100%;
-                padding-bottom: 155%;
-                box-shadow: inset 0px 0px 150px 60px rgba(0, 0, 0, 0.8);
-                border-radius: 10px;
-              } */
+              background: #fff;
+              padding: 1.5em 1.5em 6.5em 1.5em;
+              box-shadow: 0 0.2rem 1.2rem rgba(0, 0, 0, 0.2);
+              transform: scale(0.8, 0.8) rotate(5deg);
             `}
           >
             <img
               css={css`
-                width: 100%;
-                max-width: 400px;
-                max-height: 600px;
-                border-radius: 10px;
+                object-fit: cover;
+                height: 600px;
+                width: 600px;
               `}
               src={engagementPhoto}
             />
           </div>
         </div>
+        <div
+          css={css`
+            width: 100%;
+            background: url("https://media-api.xogrp.com/images/2bf4f099-92a5-4864-8780-fbfea7e8030f")
+                bottom left no-repeat,
+              url("https://media-api.xogrp.com/images/bab0d90d-7c9b-41c9-9f4f-fa4be0c10e96")
+                bottom right no-repeat,
+              url("https://media-api.xogrp.com/images/2c46cfda-8f9d-4bfc-a5a7-bd8cf803e058")
+                bottom center no-repeat,
+              url("https://media-api.xogrp.com/images/30304a82-c097-4d61-8d43-25d9d4fbfd23")
+                bottom left no-repeat,
+              url("https://media-api.xogrp.com/images/4730a441-18b2-4e8c-b2c9-8f7a48986fe7")
+                bottom right no-repeat;
+            background-size: 30%, 30%, 100%, 50%, 70%;
+            min-height: 400px;
+            min-width: 420px;
+            display: flex;
+            flex: 1;
+            align-items: center;
+          `}
+        ></div>
       </MainLayout>
     </>
   );
